a78b52583a4c314d4e205b12748238832e3c6d27
carney <carney2017>
2020-04-01 16:05:32 +0800

    1、重写冒泡排序

--- src/main/java/com/xumingwei/algorithm/sort/BubbleSort.java
+++ src/main/java/com/xumingwei/algorithm/sort/BubbleSort.java
@@ -1,7 +1,6 @@
 package com.xumingwei.algorithm.sort;
 
 import com.xumingwei.algorithm.sort.base.BaseSort;
-import org.junit.Test;
 
 import java.util.List;
 
@@ -15,13 +14,18 @@ public class BubbleSort extends BaseSort {
     @Override
     public void algorithm(List<Integer> sourceDataList, List<Integer> targetDataList) {
         int size = sourceDataList.size();
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = i + 1; j < size; j++) {
-                int a = sourceDataList.get(i);
-                int b = sourceDataList.get(j);
-                if(b < a){
-                    sourceDataList.set(i, b);
-                    sourceDataList.set(j, a);
+        //1、第一层循环，用于控制循环次数（即：循环 size 次）
+        for (int i = 0; i < size; i++){
+            //2、第二层循环，用于比较和交换。j元素代表光标所在元素
+            for(int j = 0 ; j < size - 1; j++){
+                //3、当前光标所在元素
+                int a = sourceDataList.get(j);
+                //4、当前光标的后一位元素
+                int b = sourceDataList.get(j + 1);
+                //5、若前者比后者大，则交换位置（即：较大的泡泡逐渐上浮（右移））
+                if(a > b){
+                    sourceDataList.set(j, b);
+                    sourceDataList.set(j + 1, a);
                 }
             }
         }
